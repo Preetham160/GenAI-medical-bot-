720d1328bbed22d2b8c31944896a6b255e8dfc06
Preetham160 <preetham.s@Preethamss-MacBook-Air.local>
2025-12-07 13:32:56 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GenAI-medical-bot-
+
+```bash
+    git clone https://github.com/Preetham160/GenAI-medical-bot-
+
+```bash
+conda create -n medibot python 3.10 -y
+```bash
+conda activate medibot
+``` bash 
+pip install -r requirements.txt
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="genai-medical-bot",
     version="0.1.0",
-    description="A GenAI medical bot application",
-    author="Your Name",
+    author="Preetham S",
+    author_email="preetham.bmsatish@gmail.com",
     packages=find_packages(),
-    python_requires=">=3.8",
+    install_requires=[]
 )

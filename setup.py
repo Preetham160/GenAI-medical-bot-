25cd8f8ecd75f383df6ecd6132a14f596f3bc3da
Preetham.s <preetham.s@Preethamss-MacBook-Air.local>
2025-12-07 12:21:08 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genai-medical-bot",
+    version="0.1.0",
+    description="A GenAI medical bot application",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
